58b051911bf27d06e78d07327c913a1967271d1b
SC <sc@SCdeMacBook-Pro.local>
2025-07-15 22:54:31 +0800

    [update] 2.0.5 checksum hash value

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     .binaryTarget(
       name: "WeChatOpenSDK",
       url: "https://dldir1.qq.com/WechatWebDev/opensdk/XCFramework/OpenSDK2.0.5.zip",
-      checksum: "e2607b42f51cdee59fa645a07cfac1427d350f17c2663d114c19b21dee4e851b"
+      checksum: "b2923d53b20304b27f7c54a4086d097fc938fb5a2240602c4a8f903f01ccac25"
     )
   ]
-  // checksum: swift package compute-checksum OpenSDK2.0.4.zip
+  // checksum: swift package compute-checksum OpenSDK2.0.5.zip
 )
