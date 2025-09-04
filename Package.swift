26330a6ab4aa2451fd9429f257f7c6d5de4ea107
AlexPan1992 <alex.pan@amazingtalker.com>
2025-09-04 09:37:49 +0800

    [update] Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "WeChatOpenSDK",
-      url: "https://dldir1.qq.com/WechatWebDev/opensdk/XCFramework/OpenSDK2.0.5.zip",
+      url: "https://github.com/Amazingtalker-iOS/wechat_ios_sdk/releases/download/2.0.5-pre-release/OpenSDK2.0.5.zip",
       checksum: "00e7c16d76de05cae3f96fd051b928147a17e2047169a5a6e10631a293f0dd14"
     )
   ]
